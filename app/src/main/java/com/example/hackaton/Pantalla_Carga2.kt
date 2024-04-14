361f9af0859ed356cc961838e475d9eacce279ff
Luis Alejandro Bravo Ferreira <luisalejandrobravoferreira@gmail.com>
2024-04-14 10:57:16 -0500

    Cambios Jonathan

---
+++ app/src/main/java/com/example/hackaton/Pantalla_Carga2.kt
@@ -0,0 +1,17 @@
+package com.example.hackaton
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
+class Pantalla_Carga2 : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.pantalla_carga2)
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/hackaton/Resumen_Reserva.kt
@@ -0,0 +1,20 @@
+package com.example.hackaton
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
+
+class Resumen_Reserva : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.resumen_reserva)
+        val botonVolver = findViewById<ImageButton>(R.id.Buttonx1)
+
+        botonVolver.setOnClickListener {
+            val intent = Intent (this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
